674980ea5cadd1dec67de06e73131273e5003d4c
Nirav Patel <nirav4273@gmail.com>
2022-05-29 17:58:53 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config({ path: '../.env' })
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'b11c14a5fb0626',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'heroku_0117a7eafd42e03',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: 3306,
-    host: process.env.DB_HOST || 'eu-cdbr-west-03.cleardb.net',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     operatorsAliases: false,
     define: {
@@ -27,11 +27,11 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USER || 'b11c14a5fb0626',
-    password: process.env.DB_PASSWORD || 'c72b2459',
-    database: process.env.DB_DATABASE || 'heroku_0117a7eafd42e03',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: 3306,
-    host: process.env.DB_HOST || 'eu-cdbr-west-03.cleardb.net',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     operatorsAliases: false,
     define: {
